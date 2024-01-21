24f8ca93918c5cd7dcfeb4f206e7eb2f57ebb8d8
Väinö Kurula <vaino.kurula@hotmail.com>
2024-01-21 16:07:06 +0200

    fixed cors initialization

--- app.js
+++ app.js
@@ -21,10 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/portfolio', portfolioRouter);
-app.use(cors());
+ 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
